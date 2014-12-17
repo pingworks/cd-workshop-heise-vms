a4ef48a2eec70fda33c66d347876db1c8201277f
Christoph Lukas <lukas@pingworks.de>
2014-12-17 11:29:46 +0100

    another cookbook to install java and tomcat

---
+++ cddemo-chef2/cookbooks/cddemo-chef2/metadata.rb
@@ -0,0 +1,14 @@
+name             'cddemo-chef2'
+maintainer       'Alexander Birk/Christoph Lukas'
+maintainer_email 'info@pingworks.de'
+license          'Apache License, Version 2.0'
+description      'Basic Chef Cookbook'
+long_description 'Basic Chef Cookbook'
+version '0.1'
+
+supports 'debian'
+
+# we need that so that the system will do an "apt-get update" upfront
+depends 'apt'
+depends 'tomcat'
+
---
+++ cddemo-chef2/cookbooks/cddemo-chef2/recipes/default.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook Name:: cddemo-chef1
+# Recipe:: default
+#
+# Copyright 2014 pingworks - Alexander Birk und Christoph Lukas
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe 'apt'
+
+%w(vim less git wget zip unzip).each do |p|
+  package p do
+    action :install
+  end
+end
+
+include_recipe 'tomcat'
